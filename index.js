08a170a644c73406f137b3c056fa5a8ee8734ab6
paaka vidyanand sai <paakavidyanandsai@gmail.com>
2023-07-06 13:32:15 +0000

    Super Over League

---
+++ index.js
@@ -0,0 +1,11 @@
+const element = (
+  <div>
+    <div className="bg-color">
+      <h1 className="heading">Super Over League</h1>
+      <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" />
+      <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
